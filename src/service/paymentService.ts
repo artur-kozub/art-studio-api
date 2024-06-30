e8dd4415ceaee8ca56c832dba2c1acdd451f4c6a
Arthur Kozub <kozub0246@gmail.com>
2024-06-30 18:06:36 +0300

    feat: add css for submit form, returnUrl

--- src/service/paymentService.ts
+++ src/service/paymentService.ts
@@ -45,23 +45,24 @@ class WayForPayService {
         const serviceUrl = process.env.WFP_SERVICE_URL
 
         const HTML_FORM = `
-            <form method="post" action=${this.wfpApiUrl} accept-charset="utf-8">
-                <input type="hidden" name="merchantAccount" value="${this.merchantAccount}">
-                <input type="hidden" name="merchantAuthType" value="SimpleSignature">
-                <input type="hidden" name="merchantDomainName" value="${this.merchantDomain}">
-                <input type="hidden" name="orderReference" value="${orderReference}">
-                <input type="hidden" name="orderDate" value="${orderDate}">
-                <input type="hidden" name="amount" value="${productPrice}">
-                <input type="hidden" name="currency" value="UAH">
-                <input type="hidden" name="orderTimeout" value="49000">
-                <input type="hidden" name="productName[]" value="photosession">
-                <input type="hidden" name="productPrice[]" value="${productPrice}">
-                <input type="hidden" name="productCount[]" value="${productCount}">
-                <input type="hidden" name="defaultPaymentSystem" value="card">
-                <input type="hidden" name="serviceUrl" value="${serviceUrl}api/payments/set-status">
-                <input type="hidden" name="merchantSignature" value="${merchantSignature}">
-                <input type="submit" value="Перейти до оплати">
-            </form>`
+        <form method="post" action=${this.wfpApiUrl} accept-charset="utf-8" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+            <input type="hidden" name="merchantAccount" value="${this.merchantAccount}">
+            <input type="hidden" name="merchantAuthType" value="SimpleSignature">
+            <input type="hidden" name="merchantDomainName" value="${this.merchantDomain}">
+            <input type="hidden" name="orderReference" value="${orderReference}">
+            <input type="hidden" name="orderDate" value="${orderDate}">
+            <input type="hidden" name="amount" value="${productPrice}">
+            <input type="hidden" name="currency" value="UAH">
+            <input type="hidden" name="orderTimeout" value="49000">
+            <input type="hidden" name="productName[]" value="photosession">
+            <input type="hidden" name="productPrice[]" value="${productPrice}">
+            <input type="hidden" name="productCount[]" value="${productCount}">
+            <input type="hidden" name="defaultPaymentSystem" value="card">
+            <input type="hidden" name="serviceUrl" value="${serviceUrl}api/payments/set-status">
+            <input type="hidden" name="merchantSignature" value="${merchantSignature}">
+            <input type="hidden" name="returnUrl" value="https://photostudio-booking.netlify.app">
+            <input type="submit" value="Перейти до оплати" style="width: 622px; height: 96px; border-radius: 4px; box-shadow: 0px 6px 8px rgba(0, 0, 0, 0.18); background: #b3907a; font-family: Roboto; font-style: normal; font-weight: 900; font-size: 40px; text-align: center; color: #ffffff">
+        </form>`
 
         return HTML_FORM;
     }
