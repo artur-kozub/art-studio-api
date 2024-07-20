665aea7bc85cf9ed85d637b4bcec649fa3b55387
Arthur Kozub <kozub0246@gmail.com>
2024-07-20 13:40:18 +0300

    chore: update returnUrl

--- src/service/paymentService.ts
+++ src/service/paymentService.ts
@@ -38,6 +38,7 @@ class WayForPayService {
         const merchantSignature = hmac.digest('hex');
 
         const serviceUrl = process.env.WFP_SERVICE_URL
+        const webApp = process.env.WEB_APP_URL
 
         const HTML_FORM = `
         <form method="post" action=${this.wfpApiUrl} accept-charset="utf-8" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);">
@@ -55,7 +56,7 @@ class WayForPayService {
             <input type="hidden" name="defaultPaymentSystem" value="card">
             <input type="hidden" name="serviceUrl" value="${serviceUrl}api/payments/set-status">
             <input type="hidden" name="merchantSignature" value="${merchantSignature}">
-            <input type="hidden" name="returnUrl" value="https://photostudio-booking.netlify.app">
+            <input type="hidden" name="returnUrl" value="${webApp}">
             <input type="submit" value="Перейти до оплати" style="width: 622px; height: 96px; border-radius: 4px; box-shadow: 0px 6px 8px rgba(0, 0, 0, 0.18); background: #b3907a; font-family: Roboto; font-style: normal; font-weight: 900; font-size: 40px; text-align: center; color: #ffffff">
         </form>`
 
